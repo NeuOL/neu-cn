c71cc5be2137c98a9aa37a2384ee20b8bc33643a
ele828 <dongjiemusic@qq.com>
2015-10-12 18:42:42 +0800

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,34 @@ module.exports = function(grunt) {
                 port: 8080,
                 base: './'
             }
+        },
+        sass: {                              // Task
+          dist: {                            // Target
+            options: {                       // Target options
+              style: 'expanded'
+            },
+            files: {                         // Dictionary of files
+              'assets/styles/index.css': 'assets/styles/index.scss'
+            }
+          }
+        },
+        watch: {
+          css: {
+            files: 'assets/styles/*.scss',
+            tasks: ['sass']
+          },
         }
+
     });
 
     grunt.loadNpmTasks('grunt-connect');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
     grunt.registerTask('server', [
         'connect:demo'
+    ]);
+    grunt.registerTask('default', [
+        'sass'
     ]);
 };
