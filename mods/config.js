f3cce5ec25ca113e4b09617229350f3359a682f5
ele828 <dongjiemusic@qq.com>
2015-10-13 22:08:31 +0800

    fix sub index

---
+++ mods/config.js
@@ -0,0 +1,10 @@
+seajs.config({
+  base: "./mods/",
+  alias: {
+    "jquery": "jquery.js",
+    "respond": "respond.min.js",
+    "nav": "nav.js",
+    "slide": "responsiveslides.js",
+    "smallslider": "smallslider.js"
+  }
+});
